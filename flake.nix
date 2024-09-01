4c703a975ea86c9adcce987f3f74830d6cb3645b
zramctl <zerowatts@tuta.io>
2024-09-01 11:56:11 -0400

    chore: :tada: init flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Stardust";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { };
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell
+          {
+            packages = with pkgs; [
+              nodejs_latest # NodeJS runtime + NPM
+              nodePackages_latest.pnpm # PNPM package manager
+            ];
+          };
+      }
+    );
+}
